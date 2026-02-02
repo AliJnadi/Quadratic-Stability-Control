680dc56cadef413941cceb530fdaf56d997ef4ab
Ali <a.jnadi@innopolis.university>
2026-02-02 16:55:40 +0300

    Delete all files

--- state_extractor.m
+++
@@ -1,8 +0,0 @@
-function [x, y, dx, dy] = state_extractor(state)
-    l = size(state, 1);
-    n = l / 4;
-    x = state(1: n, :);
-    y = state(n + 1: 2*n, :);
-    dx = state(2*n + 1: 3*n, :);
-    dy = state(3*n + 1: 4*n, :);
-end
--- vprint.m
+++
@@ -1,5 +0,0 @@
-function vprint(verbose, level, varargin)
-    if verbose >= level
-        fprintf(varargin{:});
-    end
-end
